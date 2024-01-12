19924574024cc1b630bf0bb84132b8ab92c3cb68
lenovo <lenovo@DESKTOP-SQVO2EM>
2024-01-13 02:52:02 +0530

    modification

--- src/test/java/Features/BaseClass.java
+++ src/test/java/Features/BaseClass.java
@@ -1,6 +1,8 @@
 package Features;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.After;
@@ -21,19 +23,30 @@ public class BaseClass {
 //		System.out.println("Running after all Scenario");		
 //		System.out.println("Running after all Scenario");		
 //	}
-	
+	WebDriver driver;
 	@Before
-	public void beforeScenario() {
+	public void beforeScenario() throws InterruptedException {
+		System.out.println("Running before Scenario");
+		System.out.println("Running before Scenario");
+		System.out.println("Running before Scenario");
+		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		System.out.println("Running before Scenario");
-		System.out.println("Running before Scenario");
-		System.out.println("Running before Scenario");
+		driver=new ChromeDriver();
+		System.out.println("chrome is lauched");
+		driver.get("https://www.selenium.dev/documentation/webdriver/browsers/edge/");
+		System.out.println("selenium dev website is opened");
+		Thread.sleep(10000);
+		driver.findElement(By.linkText("Downloads")).click();
+		System.out.println("downloads link is clicked");
+		Thread.sleep(10000);
+		driver.findElement(By.linkText("Projects"));
+		System.out.println("Projets link is clicked");
 	}
 	@After
 	public void afterScenario() {
 		System.out.println("Running after Scenario");		
 		System.out.println("Running after Scenario");		
-		System.out.println("Running after Scenario");		
+		System.out.println("Running after Scenario");
+		driver.close();
 	}
 }
